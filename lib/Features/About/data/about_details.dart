6e2af337235ca851771cc9263b0b81b7607960f9
Ravan <velmurugan1211r@gmail.com>
2024-10-28 14:15:52 +0530

    feat: resume download feature added

--- lib/Features/About/data/about_details.dart
+++ lib/Features/About/data/about_details.dart
@@ -9,7 +9,7 @@ class AboutDetails {
       "I am a dedicated and enthusiastic Flutter developer with 1.5 years of experience in software development. My expertise lies in building diverse mobile and desktop applications, as well as creating websites and web apps using Flutter. I am proficient in utilizing state management solutions such as BLoC and Provider. My technical toolkit includes experience with Firebase for backend services and local databases like SQLite and Hive for data storage. Additionally, I have worked extensively with REST API integrations to ensure seamless communication between applications and backend services. I am also well-versed in implementing Clean Architecture to maintain scalable and maintainable codebases.In addition to my development skills, I have a keen eye for design and have undertaken several design projects using Figma.";
 
   static String birthDay = "12 November 2001";
-  static String website = "https://portfolio-velmurugan.web.app/";
+  static String website = "dev-ravan.github.io";
   static String degree = "BCA";
   static String city = "Coimbatore";
   static String age = "23";
